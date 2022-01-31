2b0d1df997b7e9654153e70364f3dd856e073bc8
filipandersson3 <60388955+filipandersson3@users.noreply.github.com>
2022-01-31 09:41:58 +0100

    view funkar

--- src/Controller.java
+++ src/Controller.java
@@ -1,13 +1,67 @@
-import java.util.Arrays;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Controller {
+    private Model model;
+    private View view;
+
+    public Controller() {
+        model = new Model();
+        view = new View();
+        JFrame frame = new JFrame("Encryptor");
+        frame.setContentPane(view.getPanel1());
+        frame.setJMenuBar(view.getMenuBar());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        view.getMenuItem1().addActionListener(new NewAL());
+        view.getMenuItem().addActionListener(new SaveAL());
+        view.getMenuItem2().addActionListener(new SaveAsAL());
+        view.getMenuItem3().addActionListener(new OpenAL());
+        view.getMenuItem4().addActionListener(new ExitAL());
+    }
     public static void main(String[] args) {
-        char key = '(';
-        String message = "HEJ";
-        Model model = new Model();
-        System.out.println(model.encrypt(message, key));
-        model.save(model.encrypt(message, key),"encrypted.bin");
-        System.out.println(Arrays.toString(model.load("encrypted.bin")));
-        System.out.println(model.decrypt(model.load("encrypted.bin"), key));
+        Controller controller = new Controller();
+    }
+    private class NewAL implements ActionListener { //New file
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            view.getTextArea1().setText("");
+            view.setFilename("");
+        }
+    }
+    private class SaveAL implements ActionListener { //Save file
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String result = view.getFilename();
+            if (!result.equals("")) {
+                model.save(model.encrypt(view.getTextArea1().getText(),'('), result);
+            }
+        }
+    }
+    private class SaveAsAL implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            String result = view.getNewFilename();
+            if (!result.equals("")) {
+                model.save(model.encrypt(view.getTextArea1().getText(),'('), result);
+            }
+        }
+    }
+    private class OpenAL implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            String result = view.getNewFilename();
+            if (!result.equals("")) {
+                view.getTextArea1().setText(model.decrypt(model.load(result),'('));
+            }
+        }
+    }
+    private class ExitAL implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            System.exit(0);
+        }
     }
 }
---
+++ src/View.java
@@ -0,0 +1,92 @@
+import javax.swing.*;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+public class View {
+    private JPanel panel1;
+    private JTextArea textArea1;
+    private JScrollPane scrollPane1;
+    private String filename = "";
+    private JMenuBar menuBar;
+    private JMenu menu1;
+    private JMenuItem menuItem1;
+    private JMenuItem menuItem;
+    private JMenuItem menuItem2;
+    private JMenuItem menuItem3;
+    private JMenuItem menuItem4;
+
+
+    public View() {
+        menuBar = new JMenuBar();
+        menu1 = new JMenu("File");
+        menuItem1 = new JMenuItem("New");
+        menu1.add(menuItem1);
+        menuItem = new JMenuItem("Save");
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_MASK));
+        menu1.add(menuItem);
+        menuItem2 = new JMenuItem("Save as...");
+        menu1.add(menuItem2);
+        menuItem3 = new JMenuItem("Open...");
+        menu1.add(menuItem3);
+        menu1.addSeparator();
+        menuItem4 = new JMenuItem("Exit");
+        menu1.add(menuItem4);
+        menuBar.add(menu1);
+    }
+
+    public JMenuItem getMenuItem1() {
+        return menuItem1;
+    }
+
+    public JMenuItem getMenuItem() {
+        return menuItem;
+    }
+
+    public JMenuItem getMenuItem2() {
+        return menuItem2;
+    }
+
+    public JMenuItem getMenuItem3() {
+        return menuItem3;
+    }
+
+    public JMenuItem getMenuItem4() {
+        return menuItem4;
+    }
+
+    public JPanel getPanel1() {
+        return panel1;
+    }
+
+    public JTextArea getTextArea1() {
+        return textArea1;
+    }
+
+    public JMenuBar getMenuBar() {
+        return menuBar;
+    }
+
+    public String getFilename() {
+        if (filename.equals("")) {
+            return getNewFilename();
+        } else {
+            return filename;
+        }
+    }
+    public String getNewFilename() {
+        JFileChooser fc = new JFileChooser();
+        int result = fc.showOpenDialog(null);
+        if (result!=JFileChooser.APPROVE_OPTION){
+            JOptionPane.showConfirmDialog(null,
+                    "No file was chosen", "Error",JOptionPane.DEFAULT_OPTION,JOptionPane.ERROR_MESSAGE);
+            return "";
+        } else {
+            filename = fc.getSelectedFile().getAbsolutePath();
+            return filename;
+        }
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+}
